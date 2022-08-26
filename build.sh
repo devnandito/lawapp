7416d4401b01803ff221bd2a19e8666ec50424e5
Diego <fhersa@gmail.com>
2022-08-26 10:22:55 -0400

    update build file

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-poetry install
-
-python manage.py collectstatic --no-input
+pip install -r requirements.txt
 python manage.py migrate
+python manage.py collectstatic
