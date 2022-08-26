4058248371748de4f72ce39beb26d66ed5efd4ac
Diego <fhersa@gmail.com>
2022-08-26 13:41:01 -0400

    change options in loaders

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,13 +84,17 @@ TEMPLATES = [
         'DIRS': [
             str(APPS_DIR.path('templates')),
         ],
-        # 'APP_DIRS': True,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
             ],
         },
     },
