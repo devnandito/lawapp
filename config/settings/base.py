be4409f15c48295286f98df903908c8bfd6fe51e
Diego <fhersa@gmail.com>
2022-08-26 13:45:55 -0400

    delete options loaders in base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,10 +92,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ],
         },
     },
 ]
